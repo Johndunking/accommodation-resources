bbb1c46cb28ddd3b34cc505d582f014c4042a94f
John Duncan <johnduncan@Johns-MacBook-Pro.local>
2023-10-18 20:40:21 -0700

    debug

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
-const https = require('https')
+const port = 8000
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const fs = require('fs')
-const path = require('path')
 app.use(express.json())
 app.use(cors())
 
@@ -36,12 +34,9 @@ app.post('/completions', async (req, res) => {
 
 })
 
-const sslServer = https.createServer({
-    key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
-    cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem'))
-}, app)
 
-sslServer.listen(3443, () => console.log('Secure server on port 3443'))
+
+app.listen(PORT => 'your server is running on PORT' + PORT )
 
 
 
